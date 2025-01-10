f2b7165ff99187286f4eade2cbf98938cba4bbcb
tansit1 <tankhun006@gmail.com>
2025-01-10 13:21:15 +0700

    Add files via upload

---
+++ src/TestFirstProgram.java
@@ -0,0 +1,18 @@
+import data.*;
+
+public class TestFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("5555");
+         Point p = new Point();
+         p.x =17;
+         p.y =27 ;
+         System.out.println(p.y);
+         
+         
+         
+         
+	}
+
+}
---
+++ src/data/Point.java
@@ -0,0 +1,7 @@
+package data;
+
+public class Point {
+	public int x;
+	public int y;
+
+}
